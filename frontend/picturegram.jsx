2273d15f4992c0d33183e8a642c95b06f4d362b3
hubertli1991 <hubert.li@stern.nyu.edu>
2016-06-01 11:52:02 -0400

    update authentication some more

--- frontend/picturegram.jsx
+++ frontend/picturegram.jsx
@@ -3,6 +3,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 //Router
 var ReactRouter = require('react-router');
+var Link = require('react-router').Link;
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
@@ -14,10 +15,37 @@ var SessionStore = require('./stores/session_store');
 var SessionApiUtil = require('./util/session_api_util');
 
 var App = React.createClass({
+
+  componentDidMount: function () {
+    SessionStore.addListener(this.forceUpdate.bind(this));
+  },
+
+  greeting: function(){
+    if (SessionStore.isUserLoggedIn()) {
+
+    	return (
+    		<hgroup>
+    			<h2>Hi, {SessionStore.currentUser().username}!</h2>
+    			<input type="submit" value="logout" onClick={ SessionApiUtil.logout } />
+    		</hgroup>
+    	);
+    } else if (["/login", "/signup"].indexOf(this.props.location.pathname) === -1) {
+      return (
+        <nav>
+          <Link to="/login" activeClassName="current">Login</Link>
+          <Link to="/signup" activeClassName="current">Sign up!</Link>
+        </nav>
+      );
+    }
+  },
+
   render: function(){
     return (
         <div>
-          <header><h1>Picturegram</h1></header>
+          <header>
+            <h1>Picturegram</h1>
+            { this.greeting() }
+          </header>
           {this.props.children}
         </div>
     );
@@ -27,7 +55,8 @@ var App = React.createClass({
 var _Router = (
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-
+      <Route path="/login" component={ LoginForm } />
+      <Route path="/signup" component={ LoginForm } />
     </Route>
   </Router>
 );
