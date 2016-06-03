85ceef92cb7d36f7583e5f6677ac88bdb25e3598
hubertli1991 <hubert.li@stern.nyu.edu>
2016-06-02 21:32:07 -0400

    complete post component

--- frontend/components/PostIndex.jsx
+++ frontend/components/PostIndex.jsx
@@ -5,24 +5,32 @@ var ClientAction = require('../actions/client_actions');
 var PostIndex = React.createClass({
 
   getInititalState: function() {
-    return PostStore.all();
-  },
-
-  componentDidMount: function() {
-    PostStore.addListener(this._onChange);
-    ClientActions.fetchUserAndPosts(this.props.params.id)
+    return { posts: PostStore.all() };
   },
 
   _onChange: function() {
-
+    this.setState({ posts: PostStore.all() });
   };
+
+  componentDidMount: function() {
+    this.postStorelistener = PostStore.addListener(this._onChange);
+    ClientActions.fetchUserAndPosts(this.props.params.id)
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    ClientActions.fetchUserAndPosts(parseInt(newProps.params.id));
+  },
+
+  componentWillUnmount: function() {
+    this.postStorelistener.remove();
+  },
 
   render: function() {
     return (
       <div>
         <ul>
           {this.state.posts.map(function(post) {
-
+            return (< PostIndexItem post={post} />);
           })}
         </ul>
       </div>
