bcd9ad027748fd12af3269cd1b5ac556778e6938
Hubert Li <hubert.li@stern.nyu.edu>
2016-08-11 21:35:27 -0400

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,14 @@
 User.destroy_all
 Post.destroy_all
 
-user_1 = User.create!({ username: 'Thomsen', password: 1234567 })
-user_2 = User.create!({ username: 'batman', password: 1234567 })
-user_3 = User.create!({ username: 'Mark', password: 1234567 })
-user_4 = User.create!({ username: 'guest', password: 1234567 })
+user_1 = User.create!({ username: 'Dog Lover', password: 1234567 })
+user_2 = User.create!({ username: 'Batman', password: 1234567 })
+user_3 = User.create!({ username: 'Marc Baudry', password: 1234567 })
+user_4 = User.create!({ username: 'Guest', password: 1234567 })
 
 post = Post.create!([
   # first user's post
     { user_id: user_1.id, caption: "hello world!", image: File.open('app/assets/images/31.jpg') },
-    { user_id: user_2.id, caption: "this is my second post!", image: File.open('app/assets/images/shar_pei.jpg') },
-  # second user's posts
+  # third user's posts
     { user_id: user_3.id, caption: "I am a dog!", image: File.open('app/assets/images/second_dog_image.jpg') }
   ])
