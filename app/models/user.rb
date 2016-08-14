4443ac7bf9a25c623e6708f2994a04beeec45760
Hubert Li <hubert.li@stern.nyu.edu>
2016-08-14 05:11:05 -0400

    fix validations to allow OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   attr_reader :password
 
-  validates :username, :password_digest, :session_token, presence: true
+  validates :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
 
   validates :username, uniqueness: true
